b2b0375cb2ffc6dc3b70bfe294d9203c98ee9d0e
Luciano Machado <lurmachado@hotmail.com>
2024-03-27 19:33:51 -0300

    Projeto Ordem de Pedido C#

---
+++ Entities/Enum/﻿OrderStatus.cs
@@ -0,0 +1,12 @@
+﻿
+namespace OrdemPedidoCSharp.Entities.Enum
+{
+    enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
+
